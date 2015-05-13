0c6887a6c660af1c3d3e71a907ecb1c0a039f763
Sachith Dunatunga <neocpp@gmail.com>
2015-05-13 15:03:58 -0400

    Testing problem with BC enforcement.

--- code/assemble.m
+++ code/assemble.m
@@ -34,14 +34,12 @@ function [A, f] = assemble(mesh, nu, b, c, ffn)
 
     % face integrals
     for i=1:nf
-        if (mesh.f(i, 4) > 0 && mesh.f(i, 3) > 0)
+        nl = mesh.f(i, 1);
+        nr = mesh.f(i, 2);
+        el = mesh.f(i, 3);
+        er = mesh.f(i, 4);
+        if (el  > 0 && er > 0)
             % internal face
-            er = mesh.f(i, 4);
-            el = mesh.f(i, 3);
-            nr = mesh.f(i, 2);
-            nl = mesh.f(i, 1);
-
-            Af1 = zeros(2,2);
             vv = 0.5*[c + abs(c); c - abs(c)];
             ww = [1; -1];
             nlr = [nl, nr];
@@ -69,23 +67,73 @@ function [A, f] = assemble(mesh, nu, b, c, ffn)
 
             % same as before, looks backward but it's the right side of left element and vice versa.
             etaf = 2;
-            rfl = -etaf*0.5*nu*master.ar;
-            rfr = -etaf*0.5*nu*master.al;
+            rfl = -etaf*0.5*nu*master.ar*jl;
+            rfr = -etaf*0.5*nu*master.al*jr;
             Af3l = ww*[rfl(end); -rfl(end)]';
             Af3r = ww*[rfr(1); -rfr(1)]';
             A(nlr, nlr) = A(nlr, nlr) + Af3l + Af3r;
-
         else
             % external (boundary) face
-            if (mesh.f(i, 3) < 0)
+            if (el < 0)
                 % left boundary
+                vv = 0.5*[c - abs(c)];
+                ww = [-1];
+
+                Af1 = ww*vv';
+                A(nr, nr) = A(nr, nr) + Af1;
+
+                ern = mesh.nn(:, er);
+
+                % ugly hack to get (constant) jacobians in there.
+                jr = J(1,er);
+
+                % this looks backwards, but we are looking at the right side of the left element, and vice versa for the right element
+                Af2r = -ww*nu*master.dleft'/jr;
+
+                %A(nr, ern) = A(nr, ern) + Af2r;
+
+                % adjoint consistency
+                %A(ern, nr) = A(ern, nr) + Af2r';
+
+                % same as before, looks backward but it's the right side of left element and vice versa.
+                etaf = 2;
+                rfr = -etaf*nu*master.al*jr;
+                Af3r = ww*[-rfr(1)]';
+                % A(nr, nr) = A(nr, nr) + Af3r;
             else
                 % right boundary
+                vv = 0.5*[c + abs(c)];
+                ww = [1];
+
+                Af1 = ww*vv';
+                A(nl, nl) = A(nl, nl) + Af1;
+
+                eln = mesh.nn(:, el);
+
+                % ugly hack to get (constant) jacobians in there.
+                jl = J(1,el);
+
+                % this looks backwards, but we are looking at the right side of the left element, and vice versa for the right element
+                Af2l = -ww*nu*master.dright'/jl;
+
+                %A(nl, eln) = A(nl, eln) + Af2l;
+
+                % adjoint consistency
+                % A(eln, nl) = A(eln, nl) + Af2l';
+
+                % same as before, looks backward but it's the right side of left element and vice versa.
+                etaf = 2;
+                rfl = -etaf*nu*master.ar*jl;
+                Af3l = ww*[rfl(end);]';
+                % A(nl, nl) = A(nl, nl) + Af3l;
             end
         end
     end
 
     % boundaries
+    ww = [1; -1];
+    ll = [1; 1];
+    % A(mesh.lrn, mesh.bcnn) = ww*mesh.bcnn';
     lambdaL = mesh.bcnn(1);
     wL = mesh.lrn(1);
     A(wL, lambdaL) = 1;
--- code/solveproblem.m
+++ code/solveproblem.m
@@ -1,13 +1,22 @@
 clear all; close all;
 
-mesh = mkmesh_uniform([0, 1], 100, 1);
+mesh = mkmesh_uniform([0, 1], 3, 1);
 master = mkmaster(mesh);
 
-[A,f] = assemble(mesh, 1, 0, 0, @(x) x.*x);
-% [A,f] = assemble(mesh, 0, 0, 0.5, @(x) x);
+% [A,f] = assemble(mesh, 1, 0, 0, @(x) x.*x);
+% [A,f] = assemble(mesh, 1e-1, 0, 0.5, @(x) x);
+nu = 1e-2;
+b = 1;
+c = 0;
+fn = @(x) -2*nu + b*x.*x;
+[A,f] = assemble(mesh, nu, b, c, fn);
 u = A \ f;
 ut = u(mesh.nn(:));
 x = mesh.dgnodes(:);
 y = x.*(13 - x.^3)/12;
 % y = x.*x;
-plot(x, ut, '.b', x, y)
+% yfn_reaction = @(r, x) (exp(r*x)-exp(-r*x)) ./ (exp(r) - exp(-r));
+% y = yfn_reaction(sqrt(b/nu), x);
+yfn = @(x) x.*x;
+y = yfn(x);
+plot(x, ut, 'xr-', x, y)
