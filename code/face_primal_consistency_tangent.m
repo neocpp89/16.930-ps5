2cce621a8955a40d51b8093262433b959e3033ba
Sachith Dunatunga <neocpp@gmail.com>
2015-05-17 01:25:10 -0400

    Added consistency test.
    Also fixed bug in implementation. Is a bit of a hack though.

--- code/face_primal_consistency_tangent.m
+++ code/face_primal_consistency_tangent.m
@@ -3,6 +3,11 @@ function [AL, AR] = face_primal_consistency_tangent(left_master, right_master, l
     % element AL(1,2) should go in A_global([left element's right node], [left element's 2nd to left node]).
     % element AR(1,1) goes in A_global([right element's left node], [right element's left node])
     w = [1; -1]; % wL - wR
-    AL = -w*0.5*nu*left_master.dright'./left_jacobians;
-    AR = -w*0.5*nu*right_master.dleft'./right_jacobians;
+    
+    % hack for now...
+    jl = left_jacobians(1);
+    jr = right_jacobians(1);
+
+    AL = -w*0.5*nu*left_master.dright'./jl;
+    AR = -w*0.5*nu*right_master.dleft'./jr;
 end
