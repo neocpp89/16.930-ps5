75b8781502d328fdbc5176bd486470dd180e1760
Sachith Dunatunga <neocpp@gmail.com>
2015-05-18 11:09:01 -0400

    Added adjoint consistency function and tests.

---
+++ code/face_adjoint_consistency_tangent.m
@@ -0,0 +1,13 @@
+function [AL, AR] = face_adjoint_consistency_tangent(left_master, right_master, left_jacobians, right_jacobians, nu)
+    % the element AL(1,1) should go in A_global([left element's right node],[left element's left node]).
+    % element AL(2,1) should go in A_global([left element's right node], [left element's 2nd to left node]).
+    % element AR(1,1) goes in A_global([right element's left node], [right element's left node])
+    w = [1; -1]; % wL - wR
+    
+    % hack for now...
+    jl = left_jacobians(1);
+    jr = right_jacobians(1);
+
+    AL = -left_master.dright*0.5*nu*w'./jl;
+    AR = -right_master.dleft*0.5*nu*w'./jr;
+end
---
+++ code/testAdjointConsistencyTangents.m
@@ -0,0 +1,48 @@
+classdef testAdjointConsistencyTangents < matlab.unittest.TestCase
+    % Tests tangent matrices for convection-diffusion-reaction problems 
+
+    methods (Test)
+        function testFaceAdjointConsistencyMasterP1(testCase)
+            % single element of order 1
+            mesh = mkmesh_uniform([0, 1], 1, 1);
+            master = mkmaster(mesh);
+
+            jacobians = master.dphi'*mesh.dgnodes;
+
+            % should be the same as the master
+            testCase.verifyEqual(jacobians, ones(size(jacobians)), 'Abstol', 1e-10);
+
+            nu = 1;
+            [AL, AR] = face_adjoint_consistency_tangent(master, master, jacobians, jacobians, nu);
+            testCase.verifyEqual(AL, 0.5*nu*[1, -1; -1 1], 'Abstol', 1e-10);
+            testCase.verifyEqual(AR, 0.5*nu*[1, -1; -1 1], 'Abstol', 1e-10);
+
+            nu = 0.3;
+            [AL, AR] = face_adjoint_consistency_tangent(master, master, jacobians, jacobians, nu);
+            testCase.verifyEqual(AL, 0.5*nu*[1, -1; -1 1], 'Abstol', 1e-10);
+            testCase.verifyEqual(AR, 0.5*nu*[1, -1; -1 1], 'Abstol', 1e-10);
+        end
+
+        function testFaceAdjointConsistencyP1(testCase)
+            % single element of order 1
+            mesh = mkmesh_uniform([0, 0.5], 1, 1);
+            master = mkmaster(mesh);
+
+            jacobians = master.dphi'*mesh.dgnodes;
+
+            % should be the half the size of master
+            testCase.verifyEqual(jacobians, 0.5*ones(size(jacobians)), 'Abstol', 1e-10);
+
+            % tangent components double 
+            nu = 1;
+            [AL, AR] = face_adjoint_consistency_tangent(master, master, jacobians, jacobians, nu);
+            testCase.verifyEqual(AL, nu*[1, -1; -1 1], 'Abstol', 1e-10);
+            testCase.verifyEqual(AR, nu*[1, -1; -1 1], 'Abstol', 1e-10);
+
+            nu = 0.3;
+            [AL, AR] = face_adjoint_consistency_tangent(master, master, jacobians, jacobians, nu);
+            testCase.verifyEqual(AL, nu*[1, -1; -1 1], 'Abstol', 1e-10);
+            testCase.verifyEqual(AR, nu*[1, -1; -1 1], 'Abstol', 1e-10);
+        end
+    end
+end
